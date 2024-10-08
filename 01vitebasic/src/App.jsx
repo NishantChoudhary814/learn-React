13dcc062464dfbdcc2b45822e059bfa18ac279d1
Nishant Choudhary <nishantchoudhary814@gmail.com>
2024-10-09 03:56:33 +0530

    setup

---
+++ 01vitebasic/src/App.jsx
@@ -0,0 +1,8 @@
+import chai from "./Chai"
+function App() {
+  return (
+    <chai />
+  )
+}
+
+export default App
---
+++ 01vitebasic/src/Chai.jsx
@@ -0,0 +1,5 @@
+function chai(){
+    return (
+        <h3>Chai aur react with vite || Nishant Choudhary</h3>
+    )
+}
