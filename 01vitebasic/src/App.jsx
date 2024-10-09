760d7f891a834f29ffcfe87bec2a2784b135d7dd
Nishant Choudhary <nishantchoudhary814@gmail.com>
2024-10-09 16:34:41 +0530

    hooks

--- 01vitebasic/src/App.jsx
+++ 01vitebasic/src/App.jsx
@@ -1,7 +1,12 @@
 import chai from "./Chai"
 function App() {
+  const username ='chai aur code'
   return (
-    <chai />
+    <>
+      <chai />
+      <h1>Chai aur React {username}</h1>
+      <p>test para</p>
+    </>
   )
 }
 
--- 01vitebasic/src/main.jsx
+++ 01vitebasic/src/main.jsx
@@ -1,9 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+// import {jsx as _jsx} from 'react/jsx-runtime.js'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App | chai</h1>
+    </div>
+  );
+}
+
+const ReactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit google",
+};
+const anotherUser = "chai aur React";
+
+const reactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "click me to visit google",
+  anotherUser
+);
+
+createRoot(document.getElementById("root")).render(
+  reactElement
+);
