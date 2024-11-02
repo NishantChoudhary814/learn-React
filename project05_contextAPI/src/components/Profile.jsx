cdb62b682c321ee7fb7f8ce777d3033a9ea64246
Nishant Choudhary <nishantchoudhary814@gmail.com>
2024-11-03 04:42:35 +0530

    some changes

--- project05_contextAPI/src/components/Profile.jsx
+++ project05_contextAPI/src/components/Profile.jsx
@@ -6,7 +6,7 @@ function Profile() {
     const {user}=useContext(UserContext);
     if(!user) return <div>Please Login</div>
     
-    return <div>Welcome : {user.username}</div>
+    return <div>Welcome : {user.username} {user.password}</div>
 }
 
 export default Profile
